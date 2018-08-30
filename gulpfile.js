d03a1b208b9a2f395ead26ae65b1a0d0998a2900
anovi <mygrib@gmail.com>
2018-08-31 01:43:35 +0300

    Rearranged src and added index.js and Gulp

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var nib = require('nib');
+var invisible = require('invisible');
+var stylus = require('gulp-stylus');
+var rename = require("gulp-rename");
+// var minifyCSS = require('gulp-csso');
+// var concat = require('gulp-concat');
+// var sourcemaps = require('gulp-sourcemaps');
+
+// Get one .styl file and render
+gulp.task('stylus', function () {
+  return gulp.src('./src/css/index.styl')
+    .pipe(stylus({
+      use: nib()
+    }))
+    .pipe(rename("invisible.css"))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('compress', function () {
+  return gulp.src('./src/css/index.styl')
+    .pipe(stylus({
+      use: nib(),
+      compress: true
+    }))
+    .pipe(rename("invisible.min.css"))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('test', function () {
+  return gulp.src('./test/test-import.styl')
+    .pipe(stylus({
+      use: invisible()
+    }))
+    .pipe(rename("test-invisible.css"))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', [ 'stylus', 'compress' ]);
---
+++ index.js
@@ -0,0 +1,26 @@
+var stylus = require('stylus'),
+    path = require('path');
+
+exports = module.exports = plugin;
+
+/**
+ * Library version.
+ */
+exports.version = require(path.join(__dirname, '/package.json')).version;
+
+/**
+ * Stylus path.
+ */
+exports.path = __dirname + '/lib';
+
+/**
+ * Return the plugin callback for stylus.
+ *
+ * @return {Function}
+ * @api public
+ */
+function plugin() {
+  return function(style){
+    style.include(__dirname + '/lib');
+  };
+}
