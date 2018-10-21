b2a2b4775a1d42712bc056e6cc4fd7a000584ef3
anovi <mygrib@gmail.com>
2018-10-21 21:14:17 +0300

    Updated gulp tasks and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,41 @@
-var gulp = require('gulp');
-var nib = require('nib');
-var invisible = require('@anovi/invisible');
-var stylus = require('gulp-stylus');
-var rename = require("gulp-rename");
-var src = './lib/@anovi/invisible/index.styl'
-// var minifyCSS = require('gulp-csso');
-// var concat = require('gulp-concat');
-// var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const invisible = require('./index.js');
+const stylus = require('gulp-stylus');
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const src = './lib/@anovi/invisible/index.styl'
+const csso = require('gulp-csso');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Get one .styl file and render
-gulp.task('stylus', function () {
+gulp.task('build', function () {
   return gulp.src(src)
-    .pipe(stylus({
-      use: nib()
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
     }))
     .pipe(rename("invisible.css"))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('compress', function () {
-  return gulp.src(src)
-    .pipe(stylus({
-      use: nib(),
-      compress: true
+gulp.task('build-mini', function () {
+  return gulp.src('./dist/invisible.css')
+    .pipe(sourcemaps.init())
+    .pipe(stylus({}))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(csso({
+      restructure: false,
+      sourceMap: true,
+      debug: true
     }))
     .pipe(rename("invisible.min.css"))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist'));
 });
 
@@ -37,4 +48,4 @@ gulp.task('test', function () {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', [ 'stylus', 'compress' ]);
+gulp.task('default', [ 'build', 'build-mini' ]);
