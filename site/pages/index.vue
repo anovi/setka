aeee30a92a5e222c30e499e9ac6dcc4f8d57b5c3
anovi <mygrib@gmail.com>
2018-11-18 00:52:46 +0300

    Docs: index page

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -20,7 +20,7 @@
           </div>
 
           <div class="col-sm-4 mt-sm-4">
-            <p class="mt-0">Based on <a href="https://getbootstrap.com/docs/4.1/layout/overview/" target="_blank">Bootstrap</a> grid. Grid is customizable with Stylus variables.</p>
+            <p class="mt-0">Based on <a href="https://getbootstrap.com/docs/4.1/layout/overview/" target="_blank">Bootstrap</a> grid. Grid is customizable with Stylus variables.</p>
           </div>
 
           <div class="col-sm-4">
@@ -28,10 +28,19 @@
           </div>
 
           <div class="col-sm-4">
-            <p class="mt-0">Customization with CSS-variables.</p>
+            <p class="mt-0">Customizable with CSS-variables. Almost every values can be changed: size of grid gutter and spacers, width of container.</p>
+          </div>
+
+          <div class="w-100"></div>
+
+          <div class="col-sm-4 offset-sm-2 mt-sm-2">
+            <p class="mt-0 mt-sm-01">
+              Baseline grid is configured with css-variables and depends from line-height.
+              <a href="#" @click="$store.commit('toggleGrid')">Show baseline grid</a>.
+            </p>
           </div>
           
-          <div class="col-sm-5 offset-sm-3 mt-sm-4">
+          <div class="col-sm-5 offset-sm-1">
             <h3 class="mt-0">Why</h3>
             <p>If you like to use Bootstrap grid and css-utilities, but you don't need widgets and javascript and decoration styles from it. If you prefer Stylus over Saas. If you don't want to mess with preprocessors at all, but still want some customization.</p>
           </div>
