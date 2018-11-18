ec343fff3d15d4cd0be16dbedd0bbf70047d3da5
anovi <mygrib@gmail.com>
2018-11-18 13:58:52 +0300

    docs: spacings

--- site/pages/spacing.vue
+++ site/pages/spacing.vue
@@ -21,21 +21,21 @@
         <p>The classes are named using the format <code>{property}{sides}-{size}</code> for <code>xs</code> and <code>{property}{sides}-{breakpoint}-{size}</code> for <code>sm</code>, <code>md</code>, <code>lg</code>, and <code>xl</code>.</p>
         <p>Where sides is one of:</p>
         <p>
-        <code>mt</code>, <code>pt</code> - for classes that set margin-top or padding-top<br>
-        <code>mb</code>, <code>pb</code> - for classes that set margin-bottom or padding-bottom<br>
-        <code>ml</code>, <code>pl</code> - for classes that set margin-left or padding-left<br>
-        <code>mr</code>, <code>pr</code> - for classes that set margin-right or padding-right<br>
-        <code>mx</code>, <code>px</code> - for classes that set both *-left and *-right<br>
-        <code>my</code>, <code>py</code> - for classes that set both *-top and *-bottom
+        <code>mt</code>, <code>pt</code> — for classes that set margin-top or padding-top<br>
+        <code>mb</code>, <code>pb</code> — for classes that set margin-bottom or padding-bottom<br>
+        <code>ml</code>, <code>pl</code> — for classes that set margin-left or padding-left<br>
+        <code>mr</code>, <code>pr</code> — for classes that set margin-right or padding-right<br>
+        <code>mx</code>, <code>px</code> — for classes that set both *-left and *-right<br>
+        <code>my</code>, <code>py</code> — for classes that set both *-top and *-bottom
         </p>
         <p>
-        <code>0</code> - for classes that eliminate the margin or padding by setting it to 0<br>
-        <code>01</code> - (by default) for classes that set the margin or padding to 1 grid unit<br>
-        <code>1</code> - (by default) for classes that set the margin or padding to 1 line-height<br>
-        <code>2</code> - (by default) for classes that set the margin or padding to 2 line-height<br>
-        <code>3</code> - (by default) for classes that set the margin or padding to 3 line-height<br>
-        <code>4</code> - (by default) for classes that set the margin or padding to 4 line-height<br>
-        <code>auto</code> - for classes that set the margin to auto
+        <code>0</code> — for classes that eliminate the margin or padding by setting it to 0<br>
+        <code>01</code> — (by default) for classes that set the margin or padding to 1 grid unit<br>
+        <code>1</code> — (by default) for classes that set the margin or padding to 1 line-height<br>
+        <code>2</code> — (by default) for classes that set the margin or padding to 2 line-height<br>
+        <code>3</code> — (by default) for classes that set the margin or padding to 3 line-height<br>
+        <code>4</code> — (by default) for classes that set the margin or padding to 4 line-height<br>
+        <code>auto</code> — for classes that set the margin to auto
         </p>
     </main>
 </template>
