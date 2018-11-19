c2555ef704170528eb098498040e8dd73908ad6b
anovi <mygrib@gmail.com>
2018-11-19 11:10:05 +0300

    docs: changed h2 margins

--- site/pages/grid.vue
+++ site/pages/grid.vue
@@ -5,13 +5,13 @@
 
     <div class="row">
         <div class="col-auto">
-            <format-table :items="container" />
+            <format-table :items="container" class="m-0" />
         </div>
         <div class="col-auto">
-            <format-table :items="row" />
+            <format-table :items="row" class="m-0" />
         </div>
         <div class="col">
-            <format-table :items="cols" />
+            <format-table :items="cols" class="m-0" />
         </div>
     </div>
 
