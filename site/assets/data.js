55e9e2cb416cabfddf3afa3296fd615baa68325a
anovi <mygrib@gmail.com>
2018-12-01 22:06:13 +0300

    updated docs

--- site/assets/data.js
+++ site/assets/data.js
@@ -1,7 +1,7 @@
 export default {
-    cdnLink: 'https://cdn.jsdelivr.net/npm/setka@0.1.2/dist/setka.min.css',
-    distLink: 'https://github.com/anovi/setka/releases/download/v0.1.2/setka-0.1.2-dist.zip',
-    sourceLink: 'https://github.com/anovi/setka/archive/v0.1.2.zip',
+    cdnLink: 'https://cdn.jsdelivr.net/npm/setka@0.1.3/dist/setka.min.css',
+    distLink: 'https://github.com/anovi/setka/releases/download/v0.1.3/setka-0.1.3-dist.zip',
+    sourceLink: 'https://github.com/anovi/setka/archive/v0.1.3.zip',
     github: 'https://github.com/anovi/setka/'
 }
 
